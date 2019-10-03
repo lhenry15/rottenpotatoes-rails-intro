88cb4cc6c6247ca06eebb9cd1c4217e74d42f7f1
EC2 Default User <ec2-user@ip-172-31-24-7.us-east-2.compute.internal>
2019-10-03 08:55:03 +0000

    part 2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,13 +13,13 @@ class MoviesController < ApplicationController
   def index
     # deal with part 2 assignment
     @all_ratings = Movie.get_ratings
-    #if !(params[:ratings].nil?)
-    #check_ratings = params[:ratings]
-    #@movies = Movie.where(:rating => check_ratings.keys)
-    #else
-    #check_ratings = @all_ratings
-    @movies = Movie.where(:rating => params[:ratings])
-    #end
+    if !(params[:ratings].nil?)
+      check_ratings = params[:ratings]
+    else
+      check_ratings = @all_ratings
+    end
+    @movies = Movie.where(:rating => check_ratings.keys)
+
     
     # deal with part 1 assignment
     #if !(params[:sort].nil?)
